73152417ef2a0baba9def20dbfc6823a3ccd736e
DinoZzo99 <dinozoricic99@gmail.com>
2023-01-22 18:18:24 +0100

    finished offers page

--- icons/index.ts
+++ icons/index.ts
@@ -3,3 +3,4 @@ export { default as moon_logo } from "./moon_logo.png";
 export { default as rectangle } from "./rectangle.png";
 export { default as left_arrow } from "./left_arrow.png";
 export { default as right_arrow } from "./right_arrow.png";
+export { default as offers } from "./offers.png";
